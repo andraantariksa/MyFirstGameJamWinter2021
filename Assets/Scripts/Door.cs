4e454bc8ea257924cd5633be6cf3ffbfce8e8613
hardwin27 <hardwin27@gmail.com>
2021-01-18 11:49:42 +0800

    Init commit

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool isLocked = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Time.timeScale = 0f; 
+    }
+}
---
+++ Assets/Scripts/FloatExtraOp.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatExtraOp
+{
+    public bool FloatEqual(float a, float b, float dp)
+    {
+        dp = 1.0f / (dp * 10.0f);
+        return (Mathf.Abs(a - b) <= dp);
+    }
+}
